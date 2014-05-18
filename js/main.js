1f5998f855250ec96cf1ff1ca48d71b141c605cb
Gerardo Rodriguez <gerardor.com@gmail.com>
2014-05-18 18:13:35 -0300

    grafico completo 1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 var status = 0;
 
 var CIDADES = [
-{"nome": "São Paulo", 		"id": "cidade_saopaulo", 		"abbr": "SP"},
-{"nome": "Brasília", 		"id": "cidade_brasilia", 		"abbr": "DF"},
-{"nome": "Rio de Janeiro", 	"id": "cidade_riodejaneiro", 	"abbr": "RJ"},
-{"nome": "Fortaleza", 		"id": "cidade_fortaleza", 		"abbr": "CE"},
-{"nome": "Belo Horizonte", 	"id": "cidade_belohorizonte", 	"abbr": "MG"},
-{"nome": "Salvador", 		"id": "cidade_salvador", 		"abbr": "BA"},
-{"nome": "Recife", 			"id": "cidade_recife", 			"abbr": "PE"},
-{"nome": "Porto Alegre", 	"id": "cidade_portoalegre", 	"abbr": "RS"},
-{"nome": "Curitiba", 		"id": "cidade_curitiba", 		"abbr": "PA"},
-{"nome": "Manaus", 			"id": "cidade_manaus", 			"abbr": "AM"},
-{"nome": "Natal", 			"id": "cidade_natal", 			"abbr": "RN"},
-{"nome": "Cuiabá", 			"id": "cidade_cuiaba", 			"abbr": "MT"}];
+{"nome": "São Paulo", 		"prefix": "saopaulo", 		"abbr": "SP",	"bolaON": 320,	"bolaOFF": 159, "numVoosON": 302, "numVoosOFF": 141},
+{"nome": "Brasília", 		"prefix": "brasilia", 		"abbr": "DF",	"bolaON": 353,	"bolaOFF": 318, "numVoosON": 335, "numVoosOFF": 300},
+{"nome": "Rio de Janeiro", 	"prefix": "riodejaneiro", 	"abbr": "RJ",	"bolaON": 384,	"bolaOFF": 447, "numVoosON": 365, "numVoosOFF": 430},
+{"nome": "Fortaleza", 		"prefix": "fortaleza", 		"abbr": "CE",	"bolaON": 411,	"bolaOFF": 536, "numVoosON": 393, "numVoosOFF": 519},
+{"nome": "Belo Horizonte", 	"prefix": "belohorizonte", 	"abbr": "MG",	"bolaON": 438,	"bolaOFF": 582, "numVoosON": 420, "numVoosOFF": 564},
+{"nome": "Salvador", 		"prefix": "salvador", 		"abbr": "BA",	"bolaON": 465,	"bolaOFF": 626, "numVoosON": 446, "numVoosOFF": 609},
+{"nome": "Recife", 			"prefix": "recife", 		"abbr": "PE",	"bolaON": 491,	"bolaOFF": 677, "numVoosON": 471, "numVoosOFF": 659},
+{"nome": "Porto Alegre", 	"prefix": "portoalegre", 	"abbr": "RS",	"bolaON": 517,	"bolaOFF": 723, "numVoosON": 498, "numVoosOFF": 705},
+{"nome": "Curitiba", 		"prefix": "curitiba", 		"abbr": "PA",	"bolaON": 543,	"bolaOFF": 755, "numVoosON": 523, "numVoosOFF": 737},
+{"nome": "Manaus", 			"prefix": "manaus", 		"abbr": "AM",	"bolaON": 569,	"bolaOFF": 788, "numVoosON": 551, "numVoosOFF": 771},
+{"nome": "Natal", 			"prefix": "natal", 			"abbr": "RN",	"bolaON": 595,	"bolaOFF": 826, "numVoosON": 576, "numVoosOFF": 809},
+{"nome": "Cuiabá", 			"prefix": "cuiaba", 		"abbr": "MT",	"bolaON": 621,	"bolaOFF": 858, "numVoosON": 602, "numVoosOFF": 839}];
 
 window.onload=function(){
 	init();
@@ -66,48 +66,54 @@ function switchStatus(){
 
 	if (status==1){
 		$("img#plantas").attr("src", "images/secao2_ON.png");
-		$("#grafico1").attr("transform", "scale(1, .2)");
+		$("#secao1_ON").css("visibility", "hidden");
+		$("#secao1_OFF").css("top", 80);
 		$("#legendaGraficoEmissoes").css("top", 66);
-		//$("div#txtNomesCidadesWrapper").css("top", 75);
 		$("#legendaGraficoPlantas").css("top", 320);
 		$("#txtPasso2").css("top", 235);
-		$("#txtValoresCarbonoWrapper").css("top", 97);
-		//$("#titulo").css("height", 75).addClass("short");
+		$("#txtValoresCarbonoWrapper").css("top", 87);
 		$("#verMais").show();
 		$(".nomeCidade").hide();
-
-		/*$(".nomeCidade").each(function(i){
-			var objCidade;
-			for (i=0; i<=CIDADES.length; i++){
-				if (CIDADES[i]["id"] == $(this).attr("id")){
-					objCidade = CIDADES[i];
-					$(this).text(objCidade["abbr"]);
-					return;
-				}
-			}
-		});*/
+		$("#bolinhasWrapper img").each(function(i, bola){
+			$(bola).css("left", moveBolinhas($(bola).attr("id")));
+		});
+		$("#txtVoosWrapper p").each(function(i, numero){
+			$(numero).css("left", moveNumerosVoos($(numero).attr("id")));
+		});
 
 	}else{
 		$("img#plantas").attr("src", "images/secao2_OFF.png");
-		$("#grafico1").attr("transform", "scale(1, 1)");
+		$("#secao1_ON").css("visibility", "visible");
+		$("#secao1_OFF").css("top", 275);
 		$("#legendaGraficoEmissoes").css("top", 248);
-		//$("div#txtNomesCidadesWrapper").css("top", 150);
 		$("#legendaGraficoPlantas").css("top", 468);
 		$("#txtPasso2").css("top", 420);
 		$("#txtValoresCarbonoWrapper").css("top", 280);
-		//$("#titulo").css("height", 270).removeClass("short");
 		$("#verMais").hide();
 		$(".nomeCidade").show();
+		$("#bolinhasWrapper img").each(function(i, bola){
+			$(bola).css("left", moveBolinhas($(bola).attr("id")));
+		});
+		$("#txtVoosWrapper p").each(function(i, numero){
+			$(numero).css("left", moveNumerosVoos($(numero).attr("id")));
+		});
+	}
+}
 
-		/*$(".nomeCidade").each(function(i){
-			var objCidade;
-			for (i=0; i<=CIDADES.length; i++){
-				if (CIDADES[i]["id"] == $(this).attr("id")){
-					objCidade = CIDADES[i];
-					$(this).text(objCidade["nome"]);
-					return;
-				}
-			}
-		});*/
+function moveBolinhas(idBola){
+	var prefix = idBola.split("_")[1];
+	for (var i=0; i<CIDADES.length; i++){
+		if (prefix == CIDADES[i].prefix){
+			return status == 1?CIDADES[i].bolaOFF:CIDADES[i].bolaON;
+		}
+	}
+}
+function moveNumerosVoos(idNumero){
+	console.log(idNumero);
+	var prefix = idNumero.split("_")[1];
+	for (var i=0; i<CIDADES.length; i++){
+		if (prefix == CIDADES[i].prefix){
+			return status == 1?CIDADES[i].numVoosOFF:CIDADES[i].numVoosON;
+		}
 	}
 }
